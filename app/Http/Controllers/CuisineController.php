6d23bfd1a2518d312b1ccfca3be59fd90d8c6c2f
Oleksandr Dubovyk <struykm@yandex.ru>
2017-06-20 23:44:01 -0500

    add Order and Cuisine Controllers

---
+++ app/Http/Controllers/CuisineController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CuisineController extends Controller
+{
+    //
+    public function index(Request $request)
+    {
+        $request->user()->authorizeRoles(['cook']);
+        
+    }
+}
